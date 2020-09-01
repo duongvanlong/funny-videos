f80fbb4d4920f1f93f59e4ca9217d0caac527988
Long Duong <dvl8684@gmail.com>
2020-09-01 13:45:10 +0700

    use new template for devise

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,10 @@ module Funnyvideos
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.to_prepare do
-    Devise::SessionsController.layout "application"
-    Devise::RegistrationsController.layout "application"
-    Devise::ConfirmationsController.layout "application"
-    Devise::PasswordsController.layout "application"
+    Devise::SessionsController.layout "authentication"
+    Devise::RegistrationsController.layout "authentication"
+    Devise::ConfirmationsController.layout "authentication"
+    Devise::PasswordsController.layout "authentication"
 end
   end
 end
